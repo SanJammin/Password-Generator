5c369a8e85c7115d8d12aac1fd2e1cfa0357ffea
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-01-19 21:35:40 +0000

    Create generate password function

--- index.js
+++ index.js
@@ -8,3 +8,21 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
     "U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r",
     "s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@",
     "#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+
+let leftPassword = document.getElementById("left-password");
+let rightPassword = document.getElementById("right-password");
+
+function generatePassword() {
+    leftPassword.textContent = "";
+    rightPassword.textContent = "";
+    
+    for (let i = 0; i < 16; i++) {
+        let passwordCharacter = Math.floor(Math.random() * characters.length);
+        leftPassword.textContent += characters[passwordCharacter];
+    }
+
+    for (let i = 0; i < 16; i++) {
+        let passwordCharacter = Math.floor(Math.random() * characters.length);
+        rightPassword.textContent += characters[passwordCharacter];
+    }
+}

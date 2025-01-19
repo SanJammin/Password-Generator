c371a33f2d64530eae4f59f0e3c226e880041dd9
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-01-19 20:59:34 +0000

    Set up initial documents

---
+++ index.js
@@ -0,0 +1,10 @@
+// Create ability to set password length
+// Add "copy-on-click"
+// Toggle symbols and numbers, on/off
+// Create a light mode/dark mode function
+
+
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T",
+    "U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r",
+    "s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@",
+    "#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
